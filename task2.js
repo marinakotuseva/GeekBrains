f9fd11942c73b3d4207999222e2baf095a96a51e
Unknown <avesutok@gmail.com>
2018-06-08 14:50:40 +0300

    Damn task 2 changes

--- task2.js
+++ task2.js
@@ -6,8 +6,17 @@
     // option 1
     for (i = 0; i < s.length; i+= 1) {
         code = s.charCodeAt(i);
-        if ((code >= 65 && code <= 90) || (code >=97 && code <= 122) ) {
+        if (code >= 65 && code <= 90) {
             code += k;
+			if (code > 90) {
+				code = 65 + (code - 91)
+			}
+        }
+        if (code >=97 && code <= 122) {
+            code += k;
+			if (code > 122) {
+				code = 97 + (code - 123)
+			}
         }
         newString += String.fromCharCode(code);
     }
