dbe6f1c4eaad4d58bd25836e8ece3e8ccf18b132
Unknown <avesutok@gmail.com>
2018-06-12 21:30:09 +0300

    Cycle fix

--- task2.js
+++ task2.js
@@ -9,16 +9,15 @@
         if (code >= 65 && code <= 90) {
             code += k;
 			if (code > 90) {
-				code = 65 + (code - 91)
+				code = 65 + (code - 65)%26
 			}
         }
         if (code >=97 && code <= 122) {
             code += k;
 			if (code > 122) {
-				code = 97 + (code - 123)
+				code = 97 + (code - 97)%26
 			}
         }
         newString += String.fromCharCode(code);
     }
-    //process.stdout.write(newString);
     console.log(newString);
