623752c88bda6c80be9656a4d03e9ae9b2c02525
Unknown <avesutok@gmail.com>
2018-06-15 13:15:15 +0300

    WTF

--- task2.js
+++ task2.js
@@ -1,22 +1,20 @@
     var s = process.argv[2];
     var k = process.argv[3];
     var newString = "";
-    var i, code;
-
-    // option 1
+    var i,code;
     for (i = 0; i < s.length; i+= 1) {
         code = s.charCodeAt(i);
         if (code >= 65 && code <= 90) {
             code += k;
-			if (code > 90) {
-				code = 66 + (code - 65)%26
-			}
+            if (code > 90) {
+                code = 65 + (code - 65)%26;
+            }
         }
         if (code >=97 && code <= 122) {
             code += k;
-			if (code > 122) {
-				code = 98 + (code - 97)%26
-			}
+            if (code > 122) {
+                code = 97 + (code - 97)%26;
+            }
         }
         newString += String.fromCharCode(code);
     }
