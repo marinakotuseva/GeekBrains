08abff8742ca55903869754468b4af7a946553c4
Unknown <avesutok@gmail.com>
2018-06-15 15:02:51 +0300

    Обёрнуто в функцию

--- task2.js
+++ task2.js
@@ -1,21 +1,24 @@
     var s = process.argv[2];
     var k = process.argv[3];
-    var newString = "";
-    var i,code;
-    for (i = 0; i < s.length; i+= 1) {
-        code = s.charCodeAt(i);
-        if (code >= 65 && code <= 90) {
-            code += k;
-            if (code > 90) {
-                code = 65 + (code - 65)%26;
+    function encode(s, k){
+        var newString = "";
+        var i,code;
+        for (i = 0; i < s.length; i+= 1) {
+            code = s.charCodeAt(i);
+            if (code >= 65 && code <= 90) {
+                code += k;
+                if (code > 90) {
+                    code = 65 + (code - 65)%26;
+                }
             }
-        }
-        if (code >=97 && code <= 122) {
-            code += k;
-            if (code > 122) {
-                code = 97 + (code - 97)%26;
+            if (code >=97 && code <= 122) {
+                code += k;
+                if (code > 122) {
+                    code = 97 + (code - 97)%26;
+                }
             }
+            newString += String.fromCharCode(code);
         }
-        newString += String.fromCharCode(code);
+        return newString;
     }
-    console.log(newString);
+    console.log(encode(s,k));
